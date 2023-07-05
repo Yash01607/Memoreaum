86e22482f60d9aec5c949d7d61acb731d85411bc
Yash_01 <yashagrawal01meh@gmail.com>
2023-07-06 00:25:49 +0530

    CORS bug

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['*'],
   })
 );
 app.use('/posts', postRoutes);
